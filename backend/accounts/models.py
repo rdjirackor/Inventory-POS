06f152f242bffb26abdb8a45559d78a1e1835bc2
Richard <rdjirackor@st.ug.edu.gh>
2026-08-19 03:08:07 +0100

    Fixed related naming and created customer and order history models

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -19,7 +19,6 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     sku = models.CharField(max_length=50, unique=True)
     brand = models.CharField(max_length=100)
-    supplier = models.CharField(max_length=500)
     net_cost = models.DecimalField(max_digits=12, decimal_places=2)
     selling_price_without_tax = models.DecimalField(max_digits=12, decimal_places=2)
     discount_rate = models.DecimalField(max_digits=4, decimal_places=2, default=0)
@@ -28,20 +27,8 @@ class Product(models.Model):
     minimum_stock_level = models.PositiveIntegerField(default=0)
     image = models.ImageField()
     barcode_number = models.CharField(max_length=15, unique=True, blank=True, null=True)
-    taxes = models.ManyToManyField(TaxType, blank=True, related_name='Products')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="Category")
-
-class Supplier(models.Model):
-    name = models.CharField(max_length=200) 
-    products = models.ManyToManyField(Product, related_name="Products Supplied")
-    phone_number = models.CharField(max_length=18)
-    email = models.CharField(max_length=100)
-    address = models.CharField(max_length=1000)
-    contact_person = models.CharField(max_length=100, blank=True)
-    tax_number = models.CharField(max_length=20)
-
-
-
+    taxes = models.ManyToManyField(TaxType, blank=True, related_name='products')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     
     def get_total_taxrate(self):
         total = 0
@@ -55,13 +42,21 @@ class Supplier(models.Model):
         sale_price = self.selling_price_without_tax - discount_amount + total_tax_amount
         return sale_price
 
+class Supplier(models.Model):
+    name = models.CharField(max_length=200) 
+    products = models.ManyToManyField(Product, related_name="suppliers")
+    phone_number = models.CharField(max_length=18)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=1000)
+    contact_person = models.CharField(max_length=100, blank=True)
+    tax_number = models.CharField(max_length=20)
 
     
   
 
-class Purchase_Order(models.Model):
-    supplier = models.ManyToManyField(Supplier,related_name="Supplier")
-    items = models.ManyToManyField(Product,related_name="Products")
+class PurchaseOrder(models.Model):
+    supplier = models.ForeignKey(Supplier,on_delete=models.PROTECT, related_name="purchase_orders")
+    items = models.ManyToManyField(Product,related_name="purchase_orders")
     expected_delivery = models.DateField()
     STATUS_CHOICES = [
         ('Draft', 'Draft'),
@@ -75,3 +70,18 @@ class Purchase_Order(models.Model):
         default='Draft'
     )
 
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=150)
+    def __str__(self):
+        return self.name
+
+
+
+class OrderHistory(models.Model):
+    products = models.ManyToManyField(Product, related_name="orders")
+    customer = models.ForeignKey(Customer, related_name="orders", on_delete=models.PROTECT)
+    time_of_purchase = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"OH-{self.customer}-{self.time_of_purchase}"
