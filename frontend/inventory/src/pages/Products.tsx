7c44e6a5204fcc39dabdf16030c403bdba2ea12d
Richard <rdjirackor@st.ug.edu.gh>
2026-09-06 23:13:44 +0100

    Added fethc products upon visiting /products

--- frontend/inventory/src/pages/Products.tsx
+++ frontend/inventory/src/pages/Products.tsx
@@ -1,4 +1,34 @@
+import { useEffect, useState } from "react";
+import { getProducts } from "../apis/products";
+import type { Product } from "../interfaces/interfaces";
+
 function Products() {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] =useState("");
+    const [products, setProducts] = useState<Product[]>([]);
+    
+
+
+
+    async function fetchProducts(){
+        setError("");
+        setLoading(true);
+        try{
+            const data = await getProducts();
+            setProducts(data);
+        }
+        catch(error){
+            setError("Failed to load products");
+        }
+        finally{
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        fetchProducts()
+    },[]);
+    console.log(products);
+
 
     return (
         <div>
