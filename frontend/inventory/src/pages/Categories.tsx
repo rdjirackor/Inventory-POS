b1c3984602faee0c796bcd13a086585eff6edb52
Richard <rdjirackor@st.ug.edu.gh>
2026-09-16 06:59:00 +0100

    Added Actions Buttons on Category.tsx

--- frontend/inventory/src/pages/Categories.tsx
+++ frontend/inventory/src/pages/Categories.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getCategories } from "../apis/categories";
+import { deleteCategory, getCategories, getCategory } from "../apis/categories";
 import type { Category } from "../interfaces/interfaces";
 import { useNavigate } from "react-router-dom";
 
@@ -22,6 +22,27 @@ function Categories() {
         } finally {
             setLoading(false);
         }
+    }
+
+    async function DeleteCategory(category_id:number) {
+        try {
+            const category = await getCategory(category_id);
+            const confirmed = confirm(`Delete ${category.name}?`);
+            if (!confirmed){
+                return ;
+            }
+            await deleteCategory(category_id);
+
+            await fetchCategories();
+        }
+        catch(error){
+            if (error instanceof Error && error.message === "Protected") {
+                setError(
+                    "Cannot delete this"
+                );
+            }
+        }
+        
     }
 
     useEffect(() => {
@@ -49,18 +70,9 @@ function Categories() {
                         <tr key={category.id}>
                             <td>{category.id}</td>
                             <td>{category.name}</td>
-
                             <td>
-                                <button
-                                    onClick={() =>
-                                        navigate(
-                                            `/categories/${category.id}/edit`
-                                        )
-                                    }
-                                >
-                                    Update
-                                </button>
-                            </td>
+                                <button onClick={() => navigate(`/categories/${category.id}/edit`)}>Update</button>
+                                <button onClick={()=> DeleteCategory(category.id)}>Delete</button></td>
                         </tr>
                     ))}
                 </tbody>
